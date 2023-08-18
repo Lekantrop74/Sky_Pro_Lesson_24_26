5bcb6769207ca088ecedd49087631604194ce0cb
Korolev_Viktor <Lekantrop74@yandex.ru>
2023-08-18 17:52:54 +0500

    25.2 test

--- vehicle/tests.py
+++ vehicle/tests.py
@@ -16,22 +16,27 @@ class VehicleTestCase(APITestCase):
             'title': 'Test',
             'description': 'Test'
         }
-        response = self.client.post(
-            '/cars/',
-            data=data
+        response = self.client.post('/cars/', data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(response.json(),
+                         {'id': 1, 'milage': [], 'title': 'Test', 'description': 'Test', 'owner': None})
+
+        self.assertTrue(Car.objects.all().exists())
+
+    def test_list_car(self):
+        """Тестирование вывода списка машин"""
+
+        Car.objects.create(
+            title='list test',
+            description='list test'
         )
+
+        response = self.client.get('/cars/', )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(
-            response.status_code,
-            status.HTTP_201_CREATED
+            response.json(), [{'id': 2, 'milage': [], 'title': 'list test', 'description': 'list test', 'owner': None}]
         )
-
-        self.assertEqual(
-            response.json(),
-            {'id': 1, 'milage': [], 'title': 'Test', 'description': 'Test', 'owner': None}
-        )
-
-        self.assertTrue(
-            Car.objects.all().exists()
-        )
-
